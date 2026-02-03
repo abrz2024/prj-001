8b8226db5b7a38ec5ee6dc3cc0a8ec6f97cb169a
abrz2024 <abrz.forme@gmail.com>
2026-02-03 21:13:01 +0200

    One 001 bis

--- src/main/java/com/ex/One001.java
+++ src/main/java/com/ex/One001.java
@@ -1,4 +1,11 @@
 package com.ex;
 
 public class One001 {
+
+    public void setUp(){
+        System.out.println("set up");
+    }
+    public void tearDown(){
+        System.out.println("tearDown");
+    }
 }
