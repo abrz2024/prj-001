bc5b237187742ebe74b0179a827dd9c7d570d339
abrz2024 <abrz.forme@gmail.com>
2026-02-03 21:18:31 +0200

    F 011 (#4)
    * One 001 bis
    
    * One 002

--- src/main/java/com/ex/One001.java
+++ src/main/java/com/ex/One001.java
@@ -1,4 +1,13 @@
 package com.ex;
 
 public class One001 {
+
+    public void setUp(){
+        System.out.println("set up");
+    }
+    public void tearDown(){
+        System.out.println("tearDown");
+    }
+
+    public static void main(String[] args) {}
 }
