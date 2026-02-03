fca0b597d52763febbdca8e81073da205a915d46
abrz2024 <abrz.forme@gmail.com>
2026-02-03 19:19:16 +0200

    commit -001

--- src/main/java/com/ex/Main.java
+++ src/main/java/com/ex/Main.java
@@ -4,5 +4,7 @@ package com.ex;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("commit 001");
+        System.out.println("commit 001 - amend");
     }
 }
